0a031d8c60c893de45ab22d3e08ba09e473a29f8
Arushi <arushigarhwal@gmail.com>
2025-05-20 15:47:55 +0530

    key simulator fn

---
+++ key_simulator.py
@@ -0,0 +1,16 @@
+import time
+
+from pynput.keyboard import Controller
+
+class KeySimulator:
+    def __init__(self):
+        self.keyboard = Controller()
+        self.last_pressed = ""
+        self.last_time = time.time()
+
+    def press(self, key):
+        if time.time() - self.last_time > 0.5 or self.last_pressed != key:
+            self.keyboard.press(key.lower())
+            self.keyboard.release(key.lower())
+            self.last_time = time.time()
+            self.last_pressed = key
